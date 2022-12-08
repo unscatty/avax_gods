7be8d27a02871573a2e5783061fee7fa733c76ec
Carlos Aguilera <ckarlos_95@hotmail.es>
2022-12-07 19:49:02 -0600

    Add theme config

--- unocss.config.ts
+++ unocss.config.ts
@@ -14,6 +14,22 @@ export default defineConfig({
     ['btn', 'px-4 py-1 rounded inline-block bg-teal-700 text-white cursor-pointer hover:bg-teal-800 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
     ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
   ],
+  theme: {
+    colors: {
+      siteblack: '#131519',
+      siteDimBlack: '#191d23',
+      siteViolet: '#7f46f0',
+      siteWhite: '#9eacc7',
+    },
+    backgroundImage: {
+      astral: 'url("/images/background/astral.jpg")',
+      saiman: 'url("/images/background/saiman.jpg")',
+      eoaalien: 'url("/images/background/eoaalien.jpg")',
+      panight: 'url("/images/background/panight.jpg")',
+      heroImg: 'url("/images/background/hero-img.jpg")',
+      landing: 'url("/images/background/landing.jpg")',
+    },
+  },
   presets: [
     presetUno(),
     presetAttributify(),
@@ -24,9 +40,11 @@ export default defineConfig({
     presetTypography(),
     presetWebFonts({
       fonts: {
-        sans: 'DM Sans',
-        serif: 'DM Serif Display',
-        mono: 'DM Mono',
+        rajdhani: {
+          name: 'Rajdhani',
+          provider: 'google',
+          weights: [300, 400, 500, 600, 700],
+        },
       },
     }),
   ],
