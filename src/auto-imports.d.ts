29b500d36034921f6c86ef864b1e3aa836937b45
Carlos Aguilera <ckarlos_95@hotmail.es>
2022-12-19 14:26:24 -0600

    Rename alert info file

--- src/auto-imports.d.ts
+++ src/auto-imports.d.ts
@@ -121,7 +121,7 @@ declare global {
   const unrefElement: typeof import('@vueuse/core')['unrefElement']
   const until: typeof import('@vueuse/core')['until']
   const useActiveElement: typeof import('@vueuse/core')['useActiveElement']
-  const useAlertInfoStore: typeof import('./store/alertInfo')['useAlertInfoStore']
+  const useAlertInfoStore: typeof import('./store/alert-info')['useAlertInfoStore']
   const useArrayEvery: typeof import('@vueuse/core')['useArrayEvery']
   const useArrayFilter: typeof import('@vueuse/core')['useArrayFilter']
   const useArrayFind: typeof import('@vueuse/core')['useArrayFind']
@@ -420,7 +420,7 @@ declare module 'vue' {
     readonly unrefElement: UnwrapRef<typeof import('@vueuse/core')['unrefElement']>
     readonly until: UnwrapRef<typeof import('@vueuse/core')['until']>
     readonly useActiveElement: UnwrapRef<typeof import('@vueuse/core')['useActiveElement']>
-    readonly useAlertInfoStore: UnwrapRef<typeof import('./store/alertInfo')['useAlertInfoStore']>
+    readonly useAlertInfoStore: UnwrapRef<typeof import('./store/alert-info')['useAlertInfoStore']>
     readonly useArrayEvery: UnwrapRef<typeof import('@vueuse/core')['useArrayEvery']>
     readonly useArrayFilter: UnwrapRef<typeof import('@vueuse/core')['useArrayFilter']>
     readonly useArrayFind: UnwrapRef<typeof import('@vueuse/core')['useArrayFind']>
